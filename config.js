8cbbbf1492876581792c4f5bd7362f3949c9c3a6
tf7ugye <kasunr161@gmail.com>
2025-08-04 16:12:37 +0530

    nnew Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~QJAhhaDQ#6VDUI9WhtI-J-i2QQxU9RtiaOqPOQSJHwGFtznu_pCs'
 };
